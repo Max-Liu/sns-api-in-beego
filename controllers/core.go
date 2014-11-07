18d52b05123c75a9478a4641f213f86a37a3905b
Max Lau <forevervmax@gmail.com>
2014-11-08 00:56:46 +0800

    add some global var def

---
+++ controllers/core.go
@@ -0,0 +1,8 @@
+package controllers
+
+var fields []string
+var sortby []string = []string{"id"}
+var order []string = []string{"desc"}
+var query map[string]string = make(map[string]string)
+var limit int64 = 10
+var offset int64 = 0
